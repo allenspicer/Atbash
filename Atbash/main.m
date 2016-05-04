8bf7cd47e8c84d6e5d85174785492ca6c9273f3e
Allen Spicer <allenspicer@gmail.com>
2016-05-04 16:41:41 -0400

    Completed project
    Command Line response to input is translation.
    
     Known error: repeats forever.

--- Atbash/main.m
+++ Atbash/main.m
@@ -10,30 +10,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
-
-//        
-//        NSMutableDictionary *cipher = [NSMutableDictionary dictionaryWithObjects:alphabet forKeys:reversealphabet count:];
-//        
-//        NSCharacterSet * set = NSCharacterSet characterSetWithCharactersInString:@"a"
-//        
-//        cipher valueForKey:
         
-        
-        
-        
-//        -(BOOL) isZipCode:(NSString*)zipString{
-//            BOOL rc = NO;
-//    NSCharacterSet * set = [NSCharacterSet characterSetWithCharactersInString:@"1234567890"];
-//            set = [set invertedSet];
-//            NSRange myRange = [zipString rangeOfCharacterFromSet:set];
-//            rc = (myRange.location == NSNotFound);
-//            rc = ([zipString length]==5) && rc;
-//            return rc;
-//        }
-        
-        
-NSString * alphabetString = @"a b c d e f g h i j k l m n o p q r s t u v w x y z";
+NSString *translation = [[NSString alloc]init];
+NSString *  alphabetString = @"a b c d e f g h i j k l m n o p q r s t u v w x y z";
 NSString * reversealphabetString = @"Z Y X W V U T S R Q P O N M L K J I H G F E D C B A";
 
 NSArray *alphabetArray = [alphabetString componentsSeparatedByString:@" "];
@@ -45,27 +24,37 @@ NSMutableDictionary *cipher = [NSMutableDictionary dictionary];
 NSInteger i=0;
 while (i < numberOfLetters)
         {
-        [cipher setValue:alphabetArray[i] forKey:reverseAlphabetArray[i]];
+        [cipher setValue:reverseAlphabetArray[i] forKey:alphabetArray[i]];
         i++;
         }
         
-        NSLog(@"%@", cipher);
-                                       
+NSLog(@"%@", cipher);
 
-//        NSInteger i;
-//        
-//        for (i = 0; i < N_ENTRIES; i++) {
-//            
-//            char charValue = 'a' + i;
-//            keyArray[i] = [NSString stringWithFormat:@"%c", charValue];
-//            valueArray[i] = [NSNumber numberWithChar:charValue];
-//        }
-//        
-//        asciiDict = [NSDictionary dictionaryWithObjects:(id *)valueArray
-//                                                forKeys:(id *)keyArray count:N_ENTRIES];
-//
+//request char string from user
+char  input[20];
+scanf("%s", input);
+
+
+//go through characters with loop
+i = 0;
+        
+while (i < 20){
+    
+    NSString* characterString = [NSString stringWithFormat:@"%c", input[i]];
+    
+    if (![characterString isEqual: @""]) {
+    
+    NSString * translatedCharacterString = [cipher objectForKey:characterString];
+    
+    translation = [translation stringByAppendingString:translatedCharacterString];
+
+                 i++; }
+
+NSLog(@"%@", translation);
+}
         
 
+        
     }
     return 0;
 }
